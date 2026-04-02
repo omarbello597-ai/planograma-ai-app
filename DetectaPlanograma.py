6f992a175871bd8a6af91b18066ed7a261555441
omarbello597-ai <omarbello597@gmail.com>
2026-04-02 01:14:13 -0500

    Add files via upload

--- DetectaPlanograma.py
+++ DetectaPlanograma.py
@@ -25,7 +25,7 @@ st.markdown(f"""
 <style>
 
 [data-testid="stAppViewContainer"] {{
-    background: linear-gradient(rgba(0,0,0,0.85), rgba(0,0,0,0.95)),
+    background: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.7)),
     url("data:image/png;base64,{img_base64}");
     background-size: cover;
     background-position: center;
